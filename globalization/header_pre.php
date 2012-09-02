4e9e3973f80c6c2a686ef02587f8d997caf921e3
Keith <kmiyake@gc.cuny.edu>
2012-09-02 19:53:23 -0400

    separate bits into subdirectories for sub sites

---
+++ globalization/header_pre.php
@@ -0,0 +1,6 @@
+<?php 
+$logo_url = "/wp-content/blogs.dir/1109/files/2012/08/newPCPlogo.jpg";
+$gc_logo_url = "/files/2012/09/CUNY-GC-logo.png";
+$social_twitter = "cgscatgc";
+$social_facebook = "http://www.facebook.com/pages/The-Committee-on-Globalization-and-Social-Change/162041030521642";
+?>
