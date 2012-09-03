ff0f73760e72470662b5bb2d0756b75e6c4b1cf4
Keith <kmiyake@gc.cuny.edu>
2012-09-02 23:47:10 -0400

    rearranging snippets

--- globalization/html_css_inserts_header.php
+++ globalization/html_css_inserts_header.php
@@ -1,4 +1,4 @@
 <?php if (is_front_page() ) { ?>
-<link rel="stylesheet" href="http://raw.github.com/kaymmm/mellon-altahualpa/master/html_css_inserts_css_featured_articles.css" type="text/css" media="all">
+
 <?php } //front page ?>
-<link rel="stylesheet" href="http://raw.github.com/kaymmm/mellon-altahualpa/master/globalization/includes/html_css_inserts_css.css"  type="text/css" media="all">
+<link rel="stylesheet" href="http://kaymmm.github.com/mellon-altahualpa/globalization/includes/html_css_inserts_css.css"  type="text/css" media="all">
