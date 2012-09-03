3f8cee4c84757a525b78d9c0def1aa379d63e8f1
Keith <kmiyake@gc.cuny.edu>
2012-09-02 21:09:11 -0400

    separate center column and header/css into separate sites

--- pcp/header_post.php
+++ pcp/header_post.php
@@ -1,8 +1,5 @@
 <?php /* header post */ ?>
 <?php if (is_front_page() ) { ?>
-<div id="full-width">
-<?php bfa_widget_area('name=front_header&cells=2&align=9&align_2=3&width_1=700'); ?>
-</div>
 
 <?php } //front page ?>
 <?php /* end header post */ ?>
