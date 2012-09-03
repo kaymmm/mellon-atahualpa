177d40b8909c208853b535a9251d6e954f951ae3
Keith <kmiyake@gc.cuny.edu>
2012-09-02 21:34:20 -0400

    added scale image javascript and more splitting of files

--- globalization/header_post.php
+++ globalization/header_post.php
@@ -3,7 +3,6 @@
 <div id="full-width">
 <?php bfa_widget_area('name=front_header&cells=2&align=9&align_2=3&width_1=700'); ?>
 </div>
-
 <?php } //front page ?>
 <?php } //main site (not publications) ?>
 <?php /* end header post */ ?>
--- pcp/header_post.php
+++ pcp/header_post.php
@@ -1,5 +1,4 @@
 <?php /* header post */ ?>
 <?php if (is_front_page() ) { ?>
-
 <?php } //front page ?>
 <?php /* end header post */ ?>
