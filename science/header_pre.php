98be9396e25db50f391183086ff3eb5956f86798
Keith <kmiyake@gc.cuny.edu>
2012-09-05 15:54:09 -0400

    prep science studies files

--- science/header_pre.php
+++ science/header_pre.php
@@ -3,8 +3,32 @@
 <?php 
 $logo_url = "/wp-content/blogs.dir/1109/files/2012/08/newPCPlogo.jpg";
 $gc_logo_url = "/files/2012/09/CUNY-GC-logo.png";
-$social_twitter = "pcpatgc";
-$social_facebook = "https://www.facebook.com/placeculturepolitics";
+$social_twitter = "scistudies";
+$social_facebook = "http://www.facebook.com/pages/The-Committee-for-Interdisciplinary-Science-Studies/273683709395532";
 ?>
 
-<?php /* end header pre */ ?>
+<div id="header">
+	<div id="branding">
+		<h1 id="site-title">
+			<a href="<?php echo home_url('/'); ?>" title="<?php echo get_bloginfo('name'); ?>" rel="home"><span><?php echo get_bloginfo('name'); ?></span></a>
+		</h1>
+		<h2 id="site-description">
+			<span><?php echo get_bloginfo ( 'description' ); ?></span>
+		</h2>
+	</div><!-- #branding -->
+	<div id="sidebar-header" class="sidebar">
+		<div id="social-widget-2" class="widget Social_Widget widget-Social_Widget">
+			<div class="widget-wrap widget-inside">
+				<h3 class="widget-title">
+					Follow Us!
+				</h3>
+				<div class="socialicons">
+					<a target="_blank" href="http://twitter.com/#!/<?php echo $twitter_id; ?>" class="twitterlink"></a>
+					<a target="_blank" href="<?php echo $social_facebook; ?>" class="facebooklink"></a> 
+					<a target="_blank" href="http://www.cuny.edu" class="cuny"></a> 
+				</div>
+			</div>
+		</div>
+	</div><!-- #sidebar-header -->
+	%pages
+</div>
---
+++ science/html_css_inserts_css_pre.php
@@ -0,0 +1,5 @@
+<?php 
+$fa_height = 360; 
+$social_icons = "/files/2012/09/socialicons.png";
+$cuny_icons = "/files/2012/09/cuny_logo1.png";
+?>
