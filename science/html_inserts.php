0d8288c98d1e3719e48d4b595ce6945f4a94567d
Keith <kmiyake@gc.cuny.edu>
2012-09-13 11:14:30 -0400

    attempt to use enqueue scripts for js

--- science/html_inserts.php
+++ science/html_inserts.php
@@ -1,88 +1,15 @@
 <link rel="stylesheet" href="http://kaymmm.github.com/mellon-atahualpa/includes/html_css_inserts.css"  type="text/css" media="all">
-<?php 
-$fa_height = 380; 
-$fa_width = 750;
-?>
 <link rel="stylesheet" href="http://kaymmm.github.com/mellon-atahualpa/science/css/sciencestudies.css"  type="text/css" media="all">
 <link href='http://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
-<?php if ( is_home() ) { ?>
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.1.min.js"></script>
-<script type="text/javascript" src="http://kaymmm.github.com/mellon-atahualpa/includes/jquery.cycle.all.min.js"></script>
-<script type="text/javascript" src="http://kaymmm.github.com/mellon-atahualpa/includes/scaleimage.min.js"></script>
-<script type="text/javascript" src="http://kaymmm.github.com/mellon-atahualpa/includes/jquery.imagesloaded.min.js"></script>
-
-<script type="text/javascript">
-jQuery(document).ready(function($) {
+<?php if ( is_home() ) {
+	wp_deregister_script( 'jquery' );    
+    wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js', array(), null, false );        
+    wp_register_script( 'jquery.cycle', 'http://kaymmm.github.com/mellon-atahualpa/includes/jquery.cycle.all.min.js', array( 'jquery' ),null,false ); 
+	wp_register_script( 'jquery.imagesloaded', 'http://kaymmm.github.com/mellon-atahualpa/includes/jquery.imagesloaded.min.js', array( 'jquery' ),null,false ); 
+	wp_register_script( 'scaleimage', 'http://kaymmm.github.com/mellon-atahualpa/includes/scaleimage.min.js', array(),null,false ); 
+	wp_register_script( 'resizeimages', 'http://kaymmm.github.com/mellon-atahualpa/science/js/resize.min.js', array('jquery','jquery.cycle','jquery.imagesloaded','scaleimage'),null,false ); 
 	
-//	$( 'body' ).removeClass( 'no-js' );
+	wp_enqueue_script('resizeimages');
+?>
 
-//initialize the slider
-	$('#featured-content').cycle( {
-		slideExpr: '.featured-post',
-		speed: 300,
-		timeout: 7000,
-		pause: 1,
-		cleartypeNoBg: true,
-		pager: '#slide-thumbs',
-		containerResize: false,
-		slideResize: false,
-		fit: false,
-		fx: 'uncover',
-		next: '#slider-next',
-		prev: '#slider-prev',
-		pagerAnchorBuilder: function( idx, slide ) { 
-			return '#slide-thumbs li:eq(' + idx + ') a'; 
-    	}
-	});
-
-	//resize the images for the slider
-	$('#featured-content').imagesLoaded(function() {
-		$('img.featured-thumbnail').each(function() {
-			var originalDimensions = getOriginalDimensionsOfImg(this);
-			var tw = <?php echo $fa_width; ?>;
-			var th = <?php echo $fa_height; ?>;
-	   		var result = ScaleImage(originalDimensions.width, originalDimensions.height, tw, th, false);
-	   		$(this).css("width",result.width);
-	   		$(this).css("height",result.height);
-	   		$(this).css("left", result.targetleft);
-			$(this).css("top", result.targettop);
-			$(this).css("position","absolute");
-		});
-	});
-	$('#slider-nav').imagesLoaded(function() {
-		$('img.slider-nav-thumbnail').each(function() {
-			var originalDimensions = getOriginalDimensionsOfImg(this);
-	   		var tw = $(this).parent().width();
-			var th = $(this).parent().height();
-	   		var result = ScaleImage(originalDimensions.width, originalDimensions.height, tw, th, false);
-	   		$(this).css("width",result.width);
-	   		$(this).css("height",result.height);
-	   		$(this).css("left", result.targetleft);
-			$(this).css("top", result.targettop);
-		});
-	});
-
-	$('#hfeed').imagesLoaded(function() {
-		$('img.hentry-thumbnail').each(function() {
-			var originalDimensions = getOriginalDimensionsOfImg(this);
-			var tw = $(this).parents("div").width();
-			var th = $(this).parents("div").height();
-			var result = ScaleImage(originalDimensions.width, originalDimensions.height, tw, th, false);
-			$(this).css("width",result.width);
-			$(this).css("height",result.height);
-			$(this).css("left", result.targetleft);
-			$(this).css("top", result.targettop);
-		});
-	}); 
-});
-
-function getOriginalDimensionsOfImg(img_element) {
-	var t = new Image();
-	var result = { width: 0, height: 0 };
-	t.src = (img_element.getAttribute ? img_element.getAttribute("src") : false) || img_element.src;
-	result.width=t.width;
-	result.height=t.height;
-	return result;
-}
-</script>
 <?php } //front page ?>
---
+++ science/js/resize.js
@@ -0,0 +1,63 @@
+jQuery(document).ready(function($) {
+	
+//	$( 'body' ).removeClass( 'no-js' );
+
+//initialize the slider
+	$('#featured-content').cycle( {
+		slideExpr: '.featured-post',
+		speed: 300,
+		timeout: 7000,
+		pause: 1,
+		cleartypeNoBg: true,
+		pager: '#slide-thumbs',
+		containerResize: false,
+		slideResize: false,
+		fit: false,
+		fx: 'uncover',
+		next: '#slider-next',
+		prev: '#slider-prev',
+		pagerAnchorBuilder: function( idx, slide ) { 
+			return '#slide-thumbs li:eq(' + idx + ') a'; 
+    	}
+	});
+
+	//resize the images for the slider
+	$('#featured-content').imagesLoaded(function() {
+		$('img.featured-thumbnail').each(function() {
+			var originalDimensions = getOriginalDimensionsOfImg(this);
+			var tw = $(this).parents("div").width();
+			var th = $(this).parents("div").height();
+	   		var result = ScaleImage(originalDimensions.width, originalDimensions.height, tw, th, false);
+	   		$(this).css("width",result.width);
+	   		$(this).css("height",result.height);
+	   		$(this).css("left", result.targetleft);
+			$(this).css("top", result.targettop);
+			$(this).css("position","absolute");
+		});
+	});
+	$('#slider-nav').imagesLoaded(function() {
+		$('img.slider-nav-thumbnail').each(function() {
+			var originalDimensions = getOriginalDimensionsOfImg(this);
+	   		var tw = $(this).parent().width();
+			var th = $(this).parent().height();
+	   		var result = ScaleImage(originalDimensions.width, originalDimensions.height, tw, th, false);
+	   		$(this).css("width",result.width);
+	   		$(this).css("height",result.height);
+	   		$(this).css("left", result.targetleft);
+			$(this).css("top", result.targettop);
+		});
+	});
+
+	$('#hfeed').imagesLoaded(function() {
+		$('img.hentry-thumbnail').each(function() {
+			var originalDimensions = getOriginalDimensionsOfImg(this);
+			var tw = $(this).parents("div").width();
+			var th = $(this).parents("div").height();
+			var result = ScaleImage(originalDimensions.width, originalDimensions.height, tw, th, false);
+			$(this).css("width",result.width);
+			$(this).css("height",result.height);
+			$(this).css("left", result.targetleft);
+			$(this).css("top", result.targettop);
+		});
+	}); 
+});
