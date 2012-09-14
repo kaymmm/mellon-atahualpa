8559278b7d458bee87d453a090bfdc90d156db2d
Keith <kmiyake@gc.cuny.edu>
2012-09-14 12:11:37 -0400

    pcp javascript position slider image at half difference to the top of image

--- pcp/header_pre.php
+++ pcp/header_pre.php
@@ -1,5 +1,5 @@
 <?php /* header pre */
-$logo_url = "/wp-content/blogs.dir/1109/files/2012/08/newPCPlogo.jpg";
+$logo_url = "/wp-content/blogs.dir/1109/files/2012/09/pcp-logo.png";
 $gc_logo_url = "/files/2012/09/CUNY-GC-logo.png";
 $social_twitter = "pcpatgc";
 $social_facebook = "https://www.facebook.com/placeculturepolitics";
