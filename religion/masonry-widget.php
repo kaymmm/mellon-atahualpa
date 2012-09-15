eff39245e3369ea4cfb7299d3f47bbfdb2b40384
Keith <kmiyake@gc.cuny.edu>
2012-09-14 21:37:28 -0400

    religion masonry widget excerpts and css styling

--- religion/masonry-widget.php
+++ religion/masonry-widget.php
@@ -12,13 +12,12 @@ function better_excerpt($text) {
 		$output.='...';
 	}
 	$output='<p>'.preg_replace('/\[.*\]/', '', strip_tags($output, $allowed_tags)).'</p>';
-
 	return $output;
 }
 
 function str_replace_recursive($haystack, $needle, $endneedle) {                
 	$count = substr_count($haystack,$needle);
-	if ($count = 0) {
+	if ($count == 0) {
 		return $haystack;
 	}
 	$nlen = strlen($needle);
@@ -27,19 +26,12 @@ function str_replace_recursive($haystack, $needle, $endneedle) {
 	$endpos = strpos($haystack, $endneedle);
 	$before = substr($haystack,0,$offset);
 	$len = $endpos-$offset-$nlen;
-	//echo "###offset: $offset ::endpos: $endpos :: len: $len :: str: $before@@@";
 	$excerpt = substr($haystack,$offset+$nlen,$len);
-	//echo "!!!excerpt: $excerpt";
 	$after = substr($haystack,$endpos+$elen);
 	return $before.better_excerpt($excerpt).str_replace_recursive($after,$needle,$endneedle);
 }
 
-
-?>
-
-<div id="tumblelog" class="clearfix fluid masonry"> <!-- begin masonry container -->
-	<?php
-	$format = <<< EOM
+$format = <<< EOM
 <div class="box col1 masonry-brick">
 	{has_image}
 	<div class="post-thumbnail">
@@ -47,22 +39,30 @@ function str_replace_recursive($haystack, $needle, $endneedle) {
 	</div>
 	{/has_image}
 	<div class="post-content">
-		<div class="post-headline"><a href="#_EVENTURL">#_EVENTNAME</a></div>
+		<div class="post-headline"><a href="#_EVENTURL"><h3>#_EVENTNAME</h3></a></div>
 		<div class="post-date">#F #j #g.#i#a</div>
 		<div class="post-bodycopy">[EVENTTEXT]#_EVENTEXCERPT[/EVENTTEXT]</div>
 	</div>
 	<div class="post-footer">
-		Location: #_LOCATIONNAME | Tags: #_EVENTTAGS | Categories: #_EVENTCATEGORIES | <a href="#_EVENTURL">Permalink</a>
+		<span class="footer-loc">Location: #_LOCATIONNAME</span>
+		<span class="footer-tags">Tags: #_EVENTTAGS</span>
+		<span class="footer-cats">Categories: #_EVENTCATEGORIES</span>
+		<span class="footer-permalink"><a href="#_EVENTURL">Permalink</a></span>
 	</div>
 </div>
 EOM;
-	$theoutput = EM_Events::output(array(
+$theoutput = EM_Events::output(array(
 		'limit'=>6, 
 		'order'=>'DESC',
 		'orderby'=>'event_start_date,event_name',
 		'scope'=>'all',
 		'format'=>$format
 		));
-		echo str_replace_recursive($theoutput,'[EVENTTEXT]','[/EVENTTEXT]');
-	?>
-</div><!-- end masonry container -->
+$theoutput = str_replace_recursive($theoutput,'[EVENTTEXT]','[/EVENTTEXT]');
+
+echo <<<EOM
+<div id="tumblelog" class="clearfix fluid masonry">
+	$theoutput
+</div>
+EOM;
+?>
