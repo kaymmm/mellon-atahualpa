33222a55a269b015657500c0145bc90e9e693cbb
Keith <kmiyake@gc.cuny.edu>
2012-09-19 17:54:27 -0400

    minor pcp changes

--- pcp/center_column_preloop.php
+++ pcp/center_column_preloop.php
@@ -1,6 +1,8 @@
 <?php if (is_front_page() ) {
 	bfa_widget_area('name=front_header&cells=1&align=9');
-} //front page ?>
+?>
+<h3>Recent Posts and Announcements</h3>
+<?php } //front page ?>
 <?php if ( is_front_page() || is_archive() ) { ?>
 	<div id="tumblelog" class="clearfix fluid masonry" >
 <?php } else {
