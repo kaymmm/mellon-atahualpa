a0228b64060ecbb2e3a731c1a1f22d68c56e9b83
Keith <kmiyake@gc.cuny.edu>
2012-09-23 17:16:43 -0400

    keith full site

---
+++ km/header.php
@@ -0,0 +1,15 @@
+<?php /* header main */ ?>
+<table id="logoarea" cellpadding="0" cellspacing="0" border="0" width="100%"><tbody>
+<tr>
+<td rowspan="2" valign="middle" class="logoarea-title">
+<a href="<?php echo site_url(); ?>"><span style="font-size: 3em; font-family: 'Economica', sans-serif;"><span style="color:#FD253C;">KEITH</span><span style="color:#F9791E;">K</span><span style="color:#D2E102;">MIYAKE</span></span></a>
+</td>
+</tr>
+<tr><td valign="bottom" class="search-box" align="right">
+	<div class="searchbox"><form method="get" class="searchform" action="<?php echo site_url(); ?>">
+		<div class="searchbox-form"><input type="text" class="text inputblur" onfocus="this.value=''" value="search the site" onblur="this.value='search the site'" name="s"></div>
+	</form></div> 
+</td></tr>
+</tbody></table>
+%pages
+<?php /* end header main */ ?>
---
+++ km/inserts_header.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="http://kaymmm.github.com/mellon-atahualpa/includes/mellon.css"  type="text/css" media="all">
+<link rel="stylesheet" href="http://kaymmm.github.com/mellon-atahualpa/km/css/km.css"  type="text/css" media="all">
+<link href='http://fonts.googleapis.com/css?family=Economica|Montaga' rel='stylesheet' type='text/css'>
+
+<?php if ( is_front_page() ) {
+	wp_deregister_script( 'jquery' );    
+	wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js', array(), null, false );
+	wp_register_script( 'jquery.masonry', 'http://desandro.github.com/masonry/jquery.masonry.min.js', array('jquery'), null, false );
+	wp_register_script( 'jquery.imagesloaded', 'http://kaymmm.github.com/mellon-atahualpa/includes/jquery.imagesloaded.min.js', array( 'jquery' ),null,false ); 
+	wp_register_script( 'scaleimage', 'http://kaymmm.github.com/mellon-atahualpa/includes/scaleimage.min.js', array(),null,false ); 
+	wp_register_script( 'resizeimages', 'http://kaymmm.github.com/mellon-atahualpa/religion/js/resize.min.js', array('jquery','jquery.masonry','jquery.imagesloaded','scaleimage'),null,false ); 	
+	wp_enqueue_script('resizeimages');
+} //home ?>
