59d184af154470e8c44cadfe748dacc41dc5818d
Keith <kmiyake@gc.cuny.edu>
2012-09-24 00:36:45 -0400

    add adminbar to sticky window top

--- km/js/stickyscroll.js
+++ km/js/stickyscroll.js
@@ -2,7 +2,7 @@ function sticky_relocate() {
 	var wpadminbar_offset = $('#wpadminbar').height;
 	var window_top = $(window).scrollTop();
 	var div_top = $('#sticky-anchor').offset().top;
-	if ((window_top-wpadminbar_offset) > div_top) {
+	if ((window_top+wpadminbar_offset) > div_top) {
 		$('#logoarea').addClass('stickyscroll');
 /*		$('#header').css('top',wpadminbar_offset);
 		$('#header').css('position','fixed');*/
