207a6da75ba231e18acf2a17224384701df521ef
Keith <kmiyake@gc.cuny.edu>
2012-09-27 14:54:50 -0400

    fix pagination on pcp site

--- pcp/header_pre.php
+++ pcp/header_pre.php
@@ -1,13 +1,13 @@
 <?php /* header pre */
-$logo_url = site_url()."wp-content/blogs.dir/1109/files/2012/09/pcp-logo.png";
-$gc_logo_url = site_url()."/files/2012/09/CUNY-GC-logo.png";
+$logo_url = site_url()."/wp-content/blogs.dir/1109/files/2012/09/pcp-logo.png";
+$gc_logo_url = site_url()."/wp-content/blogs.dir/1109/files/2012/09/CUNY-GC-logo.png";
 $social_twitter = "pcpatgc";
 $social_facebook = "https://www.facebook.com/placeculturepolitics";
 
 global $wp_query;
 if (!is_singular()) {
 	$paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
-	$args = array('post_type' => array('post','event'),'scope' => 'all','paged' => $paged);
+	$args = array('post_type' => array('post','event'),'scope' => 'all','paged' => $paged,'order' => 'DESC','orderby' => 'start_date');
 	$args = array_merge($args, $wp_query->query );
 	query_posts($args);
 } 
--- pcp/html_inserts.php
+++ pcp/html_inserts.php
@@ -1,16 +1,23 @@
 <link rel="stylesheet" href="http://kaymmm.github.com/mellon-atahualpa/includes/mellon.css"  type="text/css" media="all">
 <link rel="stylesheet" href="http://kaymmm.github.com/mellon-atahualpa/pcp/css/pcp.css"  type="text/css" media="all">
 <?php	
-	wp_deregister_script( 'jquery' );
-	wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js', array(), null, false );
-	wp_register_script( 'jquery.masonry', 'http://desandro.github.com/masonry/jquery.masonry.min.js', array('jquery'), null, false );        
-	wp_register_script( 'jquery.imagesloaded', 'http://kaymmm.github.com/mellon-atahualpa/includes/jquery.imagesloaded.min.js', array( 'jquery' ),null,false ); 
-	wp_register_script( 'scaleimage', 'http://kaymmm.github.com/mellon-atahualpa/includes/scaleimage.min.js', array(),null,false ); 
-	wp_register_script( 'resizeimages', 'http://kaymmm.github.com/mellon-atahualpa/pcp/js/resize.min.js', array('jquery','jquery.masonry','jquery.imagesloaded','scaleimage'),null,false ); 
-	wp_enqueue_script('resizeimages');
-	if (is_front_page()) {
-		wp_register_script( 'jquery.cycle', 'http://kaymmm.github.com/mellon-atahualpa/includes/jquery.cycle.all.min.js', array( 'jquery' ),null,false ); 
-		wp_register_script( 'slider', 'http://kaymmm.github.com/mellon-atahualpa/pcp/js/slider.min.js', array('jquery','jquery.cycle','jquery.imagesloaded','scaleimage'),null,false ); 
-		wp_enqueue_script('slider');
-	} 
+wp_deregister_script( 'jquery' );
+wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js', array(), null, false );
+wp_register_script( 'jquery.masonry', 'http://desandro.github.com/masonry/jquery.masonry.min.js', array('jquery'), null, false );        
+wp_register_script( 'jquery.imagesloaded', 'http://kaymmm.github.com/mellon-atahualpa/includes/jquery.imagesloaded.min.js', array( 'jquery' ),null,false ); 
+wp_register_script( 'scaleimage', 'http://kaymmm.github.com/mellon-atahualpa/includes/scaleimage.min.js', array(),null,false ); 
+wp_register_script( 'resizeimages', 'http://kaymmm.github.com/mellon-atahualpa/pcp/js/resize.min.js', array('jquery','jquery.masonry','jquery.imagesloaded','scaleimage'),null,false ); 
+wp_enqueue_script('resizeimages');
+if (is_front_page()) {
+	wp_register_script( 'jquery.cycle', 'http://kaymmm.github.com/mellon-atahualpa/includes/jquery.cycle.all.min.js', array( 'jquery' ),null,false ); 
+	wp_register_script( 'slider', 'http://kaymmm.github.com/mellon-atahualpa/pcp/js/slider.min.js', array('jquery','jquery.cycle','jquery.imagesloaded','scaleimage'),null,false ); 
+	wp_enqueue_script('slider');
+} 
+if (!is_singular()) {
+	global $wp_query;
+	$paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
+	$args = array('post_type' => array('post','event'),'scope' => 'all','paged' => $paged,'order' => 'DESC', 'orderby' => 'start_date');
+	$args = array_merge($wp_query->query,$args);
+	$wp_query = new WP_Query($args);
+}
 ?>
