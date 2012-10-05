3845dd04a7a83f38e66bb94183c28d5052c50404
Keith <kmiyake@gc.cuny.edu>
2012-10-04 23:43:39 -0400

    km sync

--- km/header.php
+++ km/header.php
@@ -1,21 +1,19 @@
-<?php /* header main */ ?>
-<table id="logoarea" cellpadding="0" cellspacing="0" border="0" width="100%"><tbody>
-<tr>
-<td rowspan="2" valign="middle" class="logoarea-title">
-<a href="<?php echo site_url(); ?>"><span style="font-size: 3em; font-family: 'Economica', sans-serif;"><span style="color:#FD253C;">KEITH</span><span style="color:#F9791E;">K</span><span style="color:#D2E102;">MIYAKE</span></span></a>
-</td>
-</tr>
-<tr><td valign="bottom" class="search-box" align="right">
-	<div class="searchbox"><form method="get" class="searchform" action="<?php echo site_url(); ?>">
-		<div class="searchbox-form"><input type="text" class="text inputblur" onfocus="this.value=''" value="search the site" onblur="this.value='search the site'" name="s"></div>
-	</form></div> 
-</td></tr>
-</tbody></table>
-%pages
+<div id="logoarea">
+<div id="logoarea-title">
+<a href="<?php echo site_url(); ?>"><span id="keith">KEITH</span><span id="kay">K</span><span id="miyake">MIYAKE</span></a>
+</div>
+<div id="sticky-anchor">
+<div id="sticky-menu">%pages
+<div class="searchbox"><form method="get" class="searchform" action="<?php echo site_url(); ?>">
+	<input type="text" class="text inputblur" onfocus="this.value=''" value="search the site" onblur="this.value='search the site'" name="s"></div>
+</form></div></div></div>
 <?php /* end header main */ 
 function has_parent($post, $post_id) {
 	if ($post->ID == $post_id) return true;
 	else if ($post->post_parent == 0) return false;
 	else return has_parent(get_post($post->post_parent),$post_id);
+}
+if (has_parent($post,2899)) {
+	bfa_widget_area('name=GEH101_Nav&cells=1&align=1&before_widget=<div id="%1$s" class="subnav %2$s">&after_widget=</div>');
 }
 ?>
--- km/inserts_header.php
+++ km/inserts_header.php
@@ -14,10 +14,11 @@ wp_register_script( 'stickyscroll', 'http://kaymmm.github.com/mellon-atahualpa/k
 wp_enqueue_script('resizeimages');
 wp_enqueue_script('stickyscroll');
 
-wp_register_script( 'hoverIntent', 'http://kaymmm.github.com/superfish-reloaded/js/hoverIntent.js', array('jquery'),null,false );
-wp_register_script( 'superfish', 'http://kaymmm.github.com/superfish-reloaded/js/superfish.js', array('jquery','hoverIntent'),null,false );
-wp_enqueue_script('superfish');
+wp_register_script( 'hoverIntent', 'http://kaymmm.github.com/mellon-atahualpa/km/js/superfish/js/hoverIntent.js', array('jquery'),null,false );
+wp_register_script( 'superfish', 'http://kaymmm.github.com/mellon-atahualpa/km/js/superfish/js/superfish.js', array('jquery','hoverIntent'),null,false );
+wp_register_script( 'superfishInit', 'http://kaymmm.github.com/mellon-atahualpa/km/js/superfish-init.js', array('jquery','superfish','hoverIntent'),null,false );
+wp_enqueue_script('superfishInit');
 ?>
 
-<link rel="stylesheet" media="screen" href="http://kaymmm.github.com/superfish-reloaded/css/superfish.css" /> 
-<link rel="stylesheet" media="screen" href="http://kaymmm.github.com/superfish-reloaded/css/superfish-navbar.css" />
+<link rel="stylesheet" media="screen" href="http://kaymmm.github.com/mellon-atahualpa/km/js/superfish/css/superfish.css" /> 
+<link rel="stylesheet" media="screen" href="http://kaymmm.github.com/mellon-atahualpa/km/js/superfish/css/superfish-navbar.css" />
